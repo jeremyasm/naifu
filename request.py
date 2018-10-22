496e0c65ab5bd5f9e7c194a69846dc4677c25bf8
jeremyasm <liyaoxing1991@gmail.com>
2018-10-22 22:58:39 +0800

    Update request.py

--- request.py
+++ request.py
@@ -1,1 +1,19 @@
-import request
+# -*- coding: utf-8 -*- 
+import requests
+import sys
+
+url = sys.argv[1]
+response = requests.get(url)
+response_body = response.text
+if response_body.find('<') == -1:
+	print('it is error')
+	print(response_body)
+else:
+	if (response_body.find('北京东单') > 0) and (response_body.find('2018-10-20') > 0):
+	   # print(response_body)
+	   print('it is xml')
+	   id = url[(len(url) - 12):len(url)]
+	   filename = id+'.txt'
+	   # print(filename)
+	   with open(filename, 'w') as f:
+	      f.write(response_body)
